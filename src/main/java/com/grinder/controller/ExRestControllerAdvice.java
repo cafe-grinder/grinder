9f82a1b168ae68c4118594ca52dda64cc5de986d
기묘한 주니어 개발자 <alcuz137@gmail.com>
2024-04-30 16:36:13 +0900

    feat : 공통 부분 추가
    에러 처리 및 성공-실패 객체 생성
    
    Resolves: KAN-40

---
+++ src/main/java/com/grinder/controller/ExRestControllerAdvice.java
@@ -0,0 +1,20 @@
+package com.grinder.controller;
+
+import com.grinder.domain.dto.ErrorResult;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@Slf4j
+@RestControllerAdvice
+public class ExRestControllerAdvice {
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ErrorResult illegalExHandle(IllegalArgumentException e) {
+        log.error("[exceptionHandle] illegalExHandle", e);
+        return new ErrorResult("wrong_input", e.getMessage());
+    }
+}
