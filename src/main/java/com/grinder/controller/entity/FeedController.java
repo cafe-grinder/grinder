559df957d18deff4c3c4f83393d9e0502620cbc7
inwoo <jiw413@naver.com>
2024-05-03 16:06:46 +0900

    Feat : FeedController의 응답 DTO 변경

--- src/main/java/com/grinder/controller/entity/FeedController.java
+++ src/main/java/com/grinder/controller/entity/FeedController.java
@@ -1,6 +1,7 @@
 package com.grinder.controller.entity;
 
 import com.grinder.domain.dto.FeedDTO;
+import com.grinder.domain.dto.SuccessResult;
 import com.grinder.domain.entity.Feed;
 import com.grinder.domain.entity.Member;
 import com.grinder.service.FeedService;
@@ -21,36 +22,43 @@ public class FeedController {
     private final FeedService feedService;
 
     @PostMapping("/newfeed")
-    public ResponseEntity<FeedDTO.FeedResponseDTO> addFeed(
+    public ResponseEntity<SuccessResult> addFeed(
             Authentication authentication,
             @RequestBody FeedDTO.FeedRequestDTO request,
             @RequestPart(value = "file", required = false) MultipartFile file
     ) {
         String memberEmail = authentication.getName();
-
         Feed feed = feedService.saveFeed(request, memberEmail, file);
-        FeedDTO.FeedResponseDTO response = new FeedDTO.FeedResponseDTO(feed);
-        return ResponseEntity.ok(response);
+
+        if (feed != null) {
+            return ResponseEntity.status(HttpStatus.CREATED).body(new SuccessResult("Add Success", "추가되었습니다."));
+        } else {
+            throw new IllegalArgumentException("예상치 못한 에러가 발생했습니다.");
+        }
     }
 
     @PutMapping("/{feed_id}")
-    public ResponseEntity<FeedDTO.FeedResponseDTO> updateFeed(
+    public ResponseEntity<SuccessResult> updateFeed(
             Authentication authentication,
             @PathVariable String feed_id,
             @RequestBody FeedDTO.FeedRequestDTO request
     ) {
         String memberEmail = authentication.getName();
-        Feed feed = feedService.updateFeed(feed_id, request);
+        Feed feed = feedService.findFeed(feed_id);
         if (memberEmail.equals(feed.getMember().getEmail())) {
-            FeedDTO.FeedResponseDTO response = new FeedDTO.FeedResponseDTO(feed);
-            return ResponseEntity.ok(response);
+            feed = feedService.updateFeed(feed_id, request);
+            if (feed != null) {
+                return ResponseEntity.status(HttpStatus.CREATED).body(new SuccessResult("Update Success", "수정되었습니다."));
+            } else {
+                throw new IllegalArgumentException("예상치 못한 에러가 발생했습니다.");
+            }
         } else {    // 403에러 (회원 불일치)
             return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
         }
     }
 
     @DeleteMapping("/{feed_id}")
-    public ResponseEntity<Void> deleteFeed(
+    public ResponseEntity<SuccessResult> deleteFeed(
             Authentication authentication,
             @PathVariable String feed_id
     ) {
@@ -58,7 +66,12 @@ public class FeedController {
         Feed feed = feedService.findFeed(feed_id);
         if (memberEmail.equals(feed.getMember().getEmail())) {
             feedService.deleteFeed(feed_id);
-            return ResponseEntity.ok().build();
+            feed = feedService.findFeed(feed_id);
+            if (feed == null) {
+                return ResponseEntity.status(HttpStatus.CREATED).body(new SuccessResult("Delete Success", "삭제되었습니다."));
+            } else {
+                throw new IllegalArgumentException("예상치 못한 에러가 발생했습니다.");
+            }
         } else {    // 403에러 (회원 불일치)
             return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
         }
