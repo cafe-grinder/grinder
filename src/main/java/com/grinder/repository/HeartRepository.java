3bca9d6f6761723e8a259f39323d41840ec53a0d
inwoo <jiw413@naver.com>
2024-05-03 15:59:42 +0900

    Feat : Heart API 구현

--- src/main/java/com/grinder/repository/HeartRepository.java
+++ src/main/java/com/grinder/repository/HeartRepository.java
@@ -9,7 +9,7 @@ import java.util.List;
 
 @Repository
 public interface HeartRepository extends JpaRepository<Heart, String> {
-    void deleteByMember_MemberIdAndContentIdAndContentType(String memberId, String contentId, ContentType contentType);
-    Heart findByMember_MemberIdAndContentIdAndContentType(String memberId, String contentId, ContentType contentType);
+    void deleteByMember_EmailAndContentIdAndContentType(String memberEmail, String contentId, ContentType contentType);
+    Heart findByMember_EmailAndContentIdAndContentType(String memberEmail, String contentId, ContentType contentType);
     List<Heart> findByContentIdAndContentType(String contentId, ContentType contentType);
 }
