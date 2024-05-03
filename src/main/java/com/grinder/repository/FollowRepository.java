e385a829ee14e475a9a949abd392a86da6e3fd4b
기묘한 주니어 개발자 <alcuz137@gmail.com>
2024-05-03 14:17:38 +0900

    feat : 팔로우 API 구현
    팔로우 컨트롤러, 서비스 코드 작성
    Test 완료
    
    Resolves: KAN-55

--- src/main/java/com/grinder/repository/FollowRepository.java
+++ src/main/java/com/grinder/repository/FollowRepository.java
@@ -1,9 +1,13 @@
 package com.grinder.repository;
 
 import com.grinder.domain.entity.Follow;
+import com.grinder.domain.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface FollowRepository extends JpaRepository<Follow, Long> {
+    Optional<Follow> findByMemberAndFollowing(Member follower, Member following);
 }
--- src/test/java/com/grinder/controller/entity/FollowControllerTest.java
+++ src/test/java/com/grinder/controller/entity/FollowControllerTest.java
@@ -1,7 +1,7 @@
 package com.grinder.controller.entity;
 
-import com.grinder.domain.dto.BookmarkDTO;
-import com.grinder.service.implement.BookmarkServiceImpl;
+import com.grinder.domain.dto.FollowDTO;
+import com.grinder.service.implement.FollowServiceImpl;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -9,7 +9,6 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableHandlerMethodArgumentResolver;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -19,29 +18,31 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
+
 import java.util.ArrayList;
 import java.util.List;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @ExtendWith(MockitoExtension.class)
-class BookmarkControllerTest {
+class FollowControllerTest {
     @InjectMocks
-    private BookmarkController bookmarkController;
+    private FollowController followController;
     @Mock
-    private BookmarkServiceImpl bookmarkService;
+    private FollowServiceImpl followService;
     @Mock
     private Authentication authentication;
+
     MockMvc mockMvc;
 
     @BeforeEach
     void setUp() {
-        this.mockMvc = MockMvcBuilders.standaloneSetup(bookmarkController)
+        this.mockMvc = MockMvcBuilders.standaloneSetup(followController)
                 .setCustomArgumentResolvers(new PageableHandlerMethodArgumentResolver())
                 .build();
 
@@ -52,16 +53,16 @@ class BookmarkControllerTest {
     }
 
     @Test
-    void findAllBookmarksSlice() throws Exception {
-        List<BookmarkDTO.findAllResponse> expectedResponse = new ArrayList<>();
-        BookmarkDTO.findAllResponse thing = new BookmarkDTO.findAllResponse();
-        thing.setCafeName("스타벅스");
+    void findAllFollowingSlice() throws Exception {
+        List<FollowDTO.findAllFollowingResponse> expectedResponse = new ArrayList<>();
+        FollowDTO.findAllFollowingResponse thing = new FollowDTO.findAllFollowingResponse();
+        thing.setFollowingEmail("test2@test.com");
         expectedResponse.add(thing);
 
         Mockito.doReturn(expectedResponse)
-                .when(bookmarkService).findAllBookmarksSlice(any(String.class), any(Pageable.class));
+                .when(followService).findAllFollowingSlice(any(String.class), any(Pageable.class));
 
-        ResultActions resultActions =mockMvc.perform(get("/api/bookmark")
+        ResultActions resultActions =mockMvc.perform(get("/api/following")
                 .param("page", "0")
                 .param("size", "10")
                 .principal(authentication));
