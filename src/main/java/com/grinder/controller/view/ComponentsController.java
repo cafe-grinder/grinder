d9a7eb78cdd2adcef42f1ad3fe39e5551da2a8c4
inwoo <jiw413@naver.com>
2024-05-08 11:21:42 +0900

    Feat : Feed 백/프론트 연결

--- src/main/java/com/grinder/controller/view/ComponentsController.java
+++ src/main/java/com/grinder/controller/view/ComponentsController.java
@@ -1,15 +1,10 @@
 package com.grinder.controller.view;
 
-import com.grinder.domain.dto.BlacklistDTO;
-import com.grinder.domain.dto.FeedDTO;
-import com.grinder.domain.dto.FollowDTO;
-import com.grinder.domain.dto.MemberDTO;
+import com.grinder.domain.dto.*;
+import com.grinder.domain.entity.Comment;
 import com.grinder.domain.entity.Feed;
 import com.grinder.exception.LoginRequiredException;
-import com.grinder.service.BlacklistService;
-import com.grinder.service.FeedService;
-import com.grinder.service.FollowService;
-import com.grinder.service.MemberService;
+import com.grinder.service.*;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
@@ -18,7 +13,6 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -31,6 +25,8 @@ public class ComponentsController {
     private final FollowService followService;
     private final BlacklistService blacklistService;
     private final FeedService feedService;
+    private final CommentService commentService;
+    private final HeartService heartService;
 
     @GetMapping("/get-header")
     public String getHeader(Authentication authentication, Model model) {
@@ -81,21 +77,33 @@ public class ComponentsController {
     }
 
     @GetMapping("/get-feed")
-    public String getFeed(Authentication authentication, Model model) {
-        MemberDTO.FindMemberDTO member = null;
-        try {
-            //String email = Optional.ofNullable(authentication.getName()).orElseThrow(() -> new LoginRequiredException("로그인이 필요합니다."));
-            String email = "example1@example.com"; // TODO: 테스트용. 나중에 지우기!
-            member = new MemberDTO.FindMemberDTO(memberService.findMemberByEmail(email));
-        } catch (Exception e) {}
-        model.addAttribute("feedMember",member);
+    public String getFeed(Model model) {
+        // 멤버
+        String email = "example1@example.com"; // TODO: 테스트용. 나중에 수정하기!
+        MemberDTO.FindMemberDTO member = new MemberDTO.FindMemberDTO(memberService.findMemberByEmail(email));
+        model.addAttribute("feedMember", member);
 
+        // 피드
         List<Feed> feedList = feedService.findAllFeed();
         List<FeedDTO.FeedResponseDTO> feedResponseList = feedList.stream().map(FeedDTO.FeedResponseDTO::new).toList();
+        for (FeedDTO.FeedResponseDTO feedResponse : feedResponseList) {
+            // 부모 댓글
+            List<Comment> parentCommentList = commentService.findParentCommentList(feedResponse.getFeedId());
+            List<CommentDTO.ParentCommentResponseDTO> parentCommentResponseList = parentCommentList.stream().map(CommentDTO.ParentCommentResponseDTO::new).toList();
+
+            // 자식 댓글
+            for (CommentDTO.ParentCommentResponseDTO parentCommentResponse : parentCommentResponseList) {
+                List<Comment> childCommentList = commentService.findChildrenCommentList(parentCommentResponse.getCommentId());
+                List<CommentDTO.ChildCommentResponseDTO> childCommentResponseList = childCommentList.stream().map(CommentDTO.ChildCommentResponseDTO::new).toList();
+                parentCommentResponse.setChildCommentList(childCommentResponseList);
+            }
+        }
         model.addAttribute("feedList", feedResponseList);
 
         return "components/feed";
     }
+
+
 
     private String getEmail() {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
