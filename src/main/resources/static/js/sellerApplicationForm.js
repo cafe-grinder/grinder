ad287a850a773ea9f76042d81107ee46afb67b7e
stein1213 <rudtlstein@gmail.com>
2024-05-11 23:50:06 +0900

    Feat: 판매자 등록 api 연동
    중복 신청 확인 로직 추가
    
    Resolves: KAN-86

--- src/main/resources/static/js/sellerApplicationForm.js
+++ src/main/resources/static/js/sellerApplicationForm.js
@@ -1,11 +1,13 @@
 const inputFile = document.getElementById('file-input');
 const applyFrom = document.getElementById('apply_form');
+
+// 이미지 첨부 시 input 클릭 이벤트
 function openFileUploader() {
    inputFile.click()
 }
 
+// input에 파일 업로드시 파일명 표시
 inputFile.addEventListener('change', (e) => {
-
     const file =  e.target.files[0]
     let reader = new FileReader();
     reader.onload = (e) => {
@@ -19,6 +21,7 @@ inputFile.addEventListener('change', (e) => {
     reader.readAsDataURL(file)
 })
 
+// 판매자 신청 요청
 applyFrom.addEventListener('submit', (e) => {
     e.preventDefault();
     const cafeId = document.getElementById('save_cafe_id').value
@@ -31,15 +34,40 @@ applyFrom.addEventListener('submit', (e) => {
         body: formData
     })
             .then(response => {
-                if (!(response.status == 201)) {
-                    console.log(response)
-                    throw new Error('The request failed');
+                if (response.status === 401 || response.status === 403) {
+                    reissue();
                 }
                 return response.json()
             })
             .then(data => {
                 alert(data.message)
             })
+            .catch(error => {
+                console.error('The request failed', error)
+            })
 })
+
+// 토큰 재발급
+function reissue() {
+    let url = '/api/reissue';
+    // 기본 요청 설정
+    let fetchOptions = {
+        method: 'GET',
+    };
+    // AJAX 요청 예시
+    fetch(url, fetchOptions)
+            .then(response => {
+                return response.json();
+            })
+            .then(data => {
+                window.location.href = "/admin";
+                console.log('Success:', data);
+            })
+            .catch((error) => {
+                window.location.href = "/";
+                console.error('Error:', error);
+            });
+}
+
 
 
